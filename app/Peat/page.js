042e2c99b818158b999170fea25a69c7c1b4d24a
Peat01 <peatzaza765@gmail.com>
2026-03-01 01:46:14 +0700

    edit landing by Peat

--- app/Peat/page.js
+++ app/Peat/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function PeatProfile() {
   return (
     <main style={{ padding: 24 }}>
-      <h1>Peat's Profile</h1>
+      <h1>Team Portfolio - Developed by Peat</h1>
       <p><b>Name:</b> Peat</p>
       <p><b>Student ID:</b> 6720210048</p>
       <p><b>Skills:</b> HTML, CSS, Git</p>
